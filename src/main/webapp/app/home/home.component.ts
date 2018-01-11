09f245cb19e674b93a3d7e201dab9293d4a17f24
svl <svl@foreach.be>
2018-01-11 10:23:25 +0100

    remember me functionality

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,22 +1,36 @@
 import {Component, OnInit} from '@angular/core';
 
-import {ResponseWrapper} from '../shared';
+import {Principal, ResponseWrapper} from '../shared';
 import {Product, ProductService} from '../entities/product';
+import {JhiEventManager} from 'ng-jhipster';
 
 @Component( {
                 selector: 'jhi-home', templateUrl: './home.component.html', styleUrls: ['home.css']
 
             } )
 export class HomeComponent implements OnInit {
-
+    account: Account;
     items: Product[] = [];
 
-    constructor( private productService: ProductService ) {
+    constructor( private principal: Principal, private eventManager: JhiEventManager, private productService: ProductService ) {
     }
 
     ngOnInit() {
+        this.principal.identity().then( ( account ) => {
+            this.account = account;
+        } );
+        this.registerAuthenticationSuccess();
+
         this.productService.query( {activeOnly: true} )
             .subscribe( ( data: ResponseWrapper ) => this.items = data.json );
+    }
+
+    registerAuthenticationSuccess() {
+        this.eventManager.subscribe( 'authenticationSuccess', ( message ) => {
+            this.principal.identity().then( ( account ) => {
+                this.account = account;
+            } );
+        } );
     }
 
 }
