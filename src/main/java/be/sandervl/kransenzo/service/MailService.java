a1bb9020026889d93ffb5562bc9493e681bed6a3
sandervanloock <sandervanloock1990@hotmail.com>
2018-01-21 20:21:16 +0100

    formatted file

--- src/main/java/be/sandervl/kransenzo/service/MailService.java
+++ src/main/java/be/sandervl/kransenzo/service/MailService.java
@@ -1,7 +1,6 @@
 package be.sandervl.kransenzo.service;
 
 import be.sandervl.kransenzo.config.ApplicationProperties;
-import be.sandervl.kransenzo.domain.Customer;
 import be.sandervl.kransenzo.domain.Order;
 import be.sandervl.kransenzo.domain.User;
 import io.github.jhipster.config.JHipsterProperties;
