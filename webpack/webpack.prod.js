42a1598fa6a2b6f8b2785f0127cee0f9a109f90c
sandervanloock <sandervanloock1990@hotmail.com>
2018-03-06 17:09:13 +0100

    fix master build with https://github.com/jhipster/generator-jhipster/issues/7188

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -108,7 +108,7 @@ module.exports = webpackMerge(commonConfig({ env: ENV }), {
             minimize: true,
             debug: false
         }),
-        new WorkboxPlugin({
+        new WorkboxPlugin.GenerateSW( {
           clientsClaim: true,
           skipWaiting: true,
         }),
