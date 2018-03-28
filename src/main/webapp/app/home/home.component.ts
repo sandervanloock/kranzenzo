7c2a624118d4f96e8f3e2a82979dae9c21d4673a
sandervanloock <sandervanloock1990@hotmail.com>
2018-03-28 17:58:40 +0200

    wip carrousel

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -9,6 +9,7 @@ import {JhiEventManager} from 'ng-jhipster';
             } )
 export class HomeComponent implements OnInit {
     account: Account;
+    homepageImages: HomepageImage[];
 
     constructor( private principal: Principal, private eventManager: JhiEventManager ) {
     }
@@ -18,6 +19,11 @@ export class HomeComponent implements OnInit {
             this.account = account;
         } );
         this.registerAuthenticationSuccess();
+        this.homepageImages = [new HomepageImage( 'https://thescoopdxb.files.wordpress.com/2014/03/20140311_134512-1.jpg', 'Mijn winkel' ),
+                               new HomepageImage( 'https://upload.wikimedia.org/wikipedia/commons/2/2f/Flower_Shop%2C_Shad_Thames%2C_London_SE1_-_geograph.org.uk_-_1703738.jpg',
+                                                  'Bezoek mijn webshop', 'Deel met al je vrienden' ), new HomepageImage(
+                'https://cmkt-image-prd.global.ssl.fastly.net/0.1.0/ps/66249/1160/772/m1/fpnw/wm0/01_creative-market-flower-shop-flyer-templates-kinzi21-.jpg?1390024406&s=dac700063c35a06a6b35c084b2d6f4c6',
+                'Ook bonnen te verkrijgen' )];
 
     }
 
@@ -29,4 +35,9 @@ export class HomeComponent implements OnInit {
         } );
     }
 
+}
+
+class HomepageImage {
+    constructor( public url: string, public caption?: string, public subcaption?: string ) {
+    }
 }
