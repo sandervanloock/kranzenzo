33fe22b2891251b933312b24fdd7594f40b44ba9
sandervanloock <sandervanloock1990@hotmail.com>
2018-04-09 13:25:39 +0200

    corrected vat number

--- src/main/java/be/sandervl/kransenzo/config/Constants.java
+++ src/main/java/be/sandervl/kransenzo/config/Constants.java
@@ -14,7 +14,7 @@ public final class Constants {
     public static final String ANONYMOUS_USER = "anonymoususer";
     public static final String DEFAULT_LANGUAGE = "nl";
     public static final String CREDENTIALS_PATH = "/tmp/host/beanstalk-credentials.json";
-    public static final String VAT_NUMBER = "BE12 1234 4567 890";
+    public static final String VAT_NUMBER = "BE 71 7332 4014 6811 4";
 
     public static final ZoneId WORKING_ZONE_ID = ZoneId.of("Europe/Paris");
 
