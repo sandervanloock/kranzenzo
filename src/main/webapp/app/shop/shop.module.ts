4a7eb76a3f4623ad1edfbbd10425af02345877a1
sandervanloock <sandervanloock1990@hotmail.com>
2018-04-10 15:52:33 +0200

    home, tag overview and contact

--- src/main/webapp/app/shop/shop.module.ts
+++ src/main/webapp/app/shop/shop.module.ts
@@ -6,10 +6,11 @@ import {ProductDetailComponent} from './product-detail.component';
 import {KransenzoSharedModule} from '../shared';
 import {ProductOrderComponent, ProductOrderPopupComponent} from './order/product-order.component';
 import {CustomerAddressComponent} from './order/customer-address.component';
+import {TagOverviewComponent} from './tag-overview.component';
 
 @NgModule( {
                imports: [KransenzoSharedModule, RouterModule.forChild( SHOP_ROUTE )],
-               declarations: [OverviewComponent, ProductDetailComponent, ProductOrderComponent, ProductOrderPopupComponent, CustomerAddressComponent],
+               declarations: [OverviewComponent, ProductDetailComponent, ProductOrderComponent, ProductOrderPopupComponent, CustomerAddressComponent, TagOverviewComponent],
                entryComponents: [ProductOrderComponent]
            } )
 export class KransenzoShopModule {
