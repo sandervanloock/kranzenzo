bb1fa9608829881d9809f409d8ac33eb23085661
sandervanloock <sandervanloock1990@hotmail.com>
2018-04-13 09:29:30 +0200

    fixed vat number

--- src/main/java/be/sandervl/kransenzo/config/Constants.java
+++ src/main/java/be/sandervl/kransenzo/config/Constants.java
@@ -14,7 +14,7 @@ public final class Constants {
     public static final String ANONYMOUS_USER = "anonymoususer";
     public static final String DEFAULT_LANGUAGE = "nl";
     public static final String CREDENTIALS_PATH = "/tmp/host/beanstalk-credentials.json";
-    public static final String VAT_NUMBER = "BE 71 7332 4014 6811 4";
+    public static final String VAT_NUMBER = "BE71 7332 4014 6869";
 
     public static final ZoneId WORKING_ZONE_ID = ZoneId.of("Europe/Paris");
 
