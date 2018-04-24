d5e44b349406c449beccc9295e435ca726514daa
sandervanloock <sandervanloock1990@hotmail.com>
2018-04-24 14:33:48 +0200

    new vat number

--- src/main/java/be/sandervl/kransenzo/config/Constants.java
+++ src/main/java/be/sandervl/kransenzo/config/Constants.java
@@ -14,7 +14,7 @@ public final class Constants {
     public static final String ANONYMOUS_USER = "anonymoususer";
     public static final String DEFAULT_LANGUAGE = "nl";
     public static final String CREDENTIALS_PATH = "/tmp/host/beanstalk-credentials.json";
-    public static final String VAT_NUMBER = "BE71 7332 4014 6869";
+    public static final String VAT_NUMBER = "BE51 9733 4667 1162";
 
     public static final ZoneId WORKING_ZONE_ID = ZoneId.of("Europe/Paris");
 
