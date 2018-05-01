ef33c4f31598d11a6106daf74a5b67be78c1309c
sandervanloock <sandervanloock1990@hotmail.com>
2018-05-01 15:41:44 +0200

    seo improvements

--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -1,13 +1,12 @@
 import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
-import {ShareModule} from '@ngx-share/core';
 
 import {KransenzoSharedModule} from '../shared';
 
 import {HOME_ROUTE, HomeComponent} from './';
 
 @NgModule({
-              imports: [KransenzoSharedModule, RouterModule.forChild( HOME_ROUTE ), ShareModule.forRoot()], declarations: [HomeComponent], entryComponents: [],
+              imports: [KransenzoSharedModule, RouterModule.forChild( HOME_ROUTE )], declarations: [HomeComponent], entryComponents: [],
     providers: [
     ],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
