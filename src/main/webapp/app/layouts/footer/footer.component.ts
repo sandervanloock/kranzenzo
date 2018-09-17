5a9a76b88b8ed3b569eaefdf0911eaaacbc48016
sandervanloock <sandervanloock1990@hotmail.com>
2018-09-17 21:01:18 +0200

    fix yarn production build 2

--- src/main/webapp/app/layouts/footer/footer.component.ts
+++ src/main/webapp/app/layouts/footer/footer.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'jhi-footer',
     templateUrl: './footer.component.html',
     styleUrls: ['footer.css']
 })
-export class FooterComponent {}
+export class FooterComponent implements OnInit {
+    currentYear: number;
+
+    ngOnInit(): void {
+        this.currentYear = new Date().getFullYear();
+    }
+}
