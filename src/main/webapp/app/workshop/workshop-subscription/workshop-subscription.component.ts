49be4c06cbe9c78d3e0c80c4ee2e40b28df524c6
Sander <svl@foreach.be>
2018-10-28 13:58:38 +0100

    workshop material design

--- src/main/webapp/app/workshop/workshop-subscription/workshop-subscription.component.ts
+++ src/main/webapp/app/workshop/workshop-subscription/workshop-subscription.component.ts
@@ -1,17 +1,16 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { ProductOrderComponent } from 'app/shop/order/product-order.component';
-import { NgbActiveModal, NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { IUser, User } from 'app/core';
 import { Workshop } from 'app/shared/model/workshop.model';
 import { WorkshopDate } from 'app/shared/model/workshop-date.model';
 import { WorkshopSubscriptionService } from 'app/entities/workshop-subscription';
 import { SubscriptionState, WorkshopSubscription } from 'app/shared/model/workshop-subscription.model';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
     selector: 'jhi-workshop-subscription',
     templateUrl: './workshop-subscription.component.html',
-    styles: []
+    styleUrls: ['workshop-subscription.css']
 })
 export class WorkshopSubscriptionComponent implements OnInit {
     user: IUser = new User();
@@ -21,14 +20,13 @@ export class WorkshopSubscriptionComponent implements OnInit {
     constructor(
         private route: ActivatedRoute,
         private workshopSubscriptionService: WorkshopSubscriptionService,
-        public activeModal: NgbActiveModal
-    ) {}
-
-    ngOnInit() {
-        this.route.children[2].params.subscribe(params => {
-            this.load(parseInt(params['date'], 10));
-        });
+        private snackBar: MatSnackBar
+    ) {
+        this.workshop = this.route.snapshot.data.workshop;
+        this.load(parseInt(this.route.snapshot.params['date'], 10));
     }
+
+    ngOnInit(): void {}
 
     submitForm() {
         const subscription = new WorkshopSubscription();
@@ -36,47 +34,19 @@ export class WorkshopSubscriptionComponent implements OnInit {
         subscription.workshopId = this.workshopDate.id;
         subscription.user = this.user;
 
-        this.workshopSubscriptionService.create(subscription).subscribe();
-
-        this.activeModal.close();
+        this.workshopSubscriptionService.create(subscription).subscribe(
+            () => {
+                const snackBarRef = this.snackBar.open('Inschrijving gelukt, bekijk je email voor de bevestiging');
+            },
+            error => {
+                const snackBarRef = this.snackBar.open('Er ging iets, probeer later opnieuw of contacteer annemie.rousseau@telenet.be');
+            }
+        );
     }
 
     load(id: number) {
         this.workshopDate = this.workshop.dates.find(date => {
             return date.id === id;
         });
-    }
-}
-
-@Component({
-    selector: 'jhi-workshop-subscription-popup',
-    template: ''
-})
-export class WorkshopSubscriptionPopupComponent implements OnInit, OnDestroy {
-    private ngbModalRef: NgbModalRef;
-
-    constructor(private activatedRoute: ActivatedRoute, private router: Router, private modalService: NgbModal) {}
-
-    ngOnInit() {
-        this.activatedRoute.data.subscribe(({ workshop }) => {
-            setTimeout(() => {
-                this.ngbModalRef = this.modalService.open(WorkshopSubscriptionComponent as Component, { size: 'lg', backdrop: 'static' });
-                this.ngbModalRef.componentInstance.workshop = workshop;
-                this.ngbModalRef.result.then(
-                    result => {
-                        this.router.navigate([{ outlets: { popup: null } }], { replaceUrl: true, queryParamsHandling: 'merge' });
-                        this.ngbModalRef = null;
-                    },
-                    reason => {
-                        this.router.navigate([{ outlets: { popup: null } }], { replaceUrl: true, queryParamsHandling: 'merge' });
-                        this.ngbModalRef = null;
-                    }
-                );
-            }, 0);
-        });
-    }
-
-    ngOnDestroy() {
-        this.ngbModalRef = null;
     }
 }
--- src/main/webapp/app/workshop/workshop.route.ts
+++ src/main/webapp/app/workshop/workshop.route.ts
@@ -1,7 +1,7 @@
 import { Route } from '@angular/router';
 import { WorkshopDetailComponent } from './workshop-detail/workshop-detail.component';
-import { WorkshopSubscriptionPopupComponent } from './workshop-subscription/workshop-subscription.component';
 import { WorkshopResolve } from 'app/entities/workshop';
+import { WorkshopSubscriptionComponent } from 'app/workshop/workshop-subscription/workshop-subscription.component';
 
 export const SHOP_ROUTE: Route[] = [
     {
@@ -14,14 +14,13 @@ export const SHOP_ROUTE: Route[] = [
     },
     {
         path: 'kranzenzo-workshop/:id/subscription/:date',
-        component: WorkshopSubscriptionPopupComponent,
+        component: WorkshopSubscriptionComponent,
         resolve: {
             workshop: WorkshopResolve
         },
         data: {
             authorities: [],
             pageTitle: 'home.title'
-        },
-        outlet: 'popup'
+        }
     }
 ];
