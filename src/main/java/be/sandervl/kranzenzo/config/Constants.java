9695e536a1dfbd0e4855e3f2cfee945ae5ee895a
svl@foreach.be <svl@foreach.be>
2020-03-31 18:37:33 +0200

    additional comment

--- src/main/java/be/sandervl/kranzenzo/config/Constants.java
+++ src/main/java/be/sandervl/kranzenzo/config/Constants.java
@@ -13,6 +13,7 @@ public final class Constants {
     public static final String SYSTEM_ACCOUNT = "system";
     public static final String ANONYMOUS_USER = "anonymoususer";
     public static final String DEFAULT_LANGUAGE = "nl";
+    //mountend volume of node in docker container (see Dockerrun.aws.json and .ebextensions/credentials.config)
     public static final String CREDENTIALS_PATH = "/tmp/host/beanstalk-credentials.json";
     public static final String VAT_NUMBER = "BE51 9733 4667 1162";
     public static final float PRICE_FOR_BATTERIES = 0.5f;
