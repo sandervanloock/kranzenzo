57ceb425a2a013ae2be8f6be14403dffed4f15c2
svl@foreach.be <svl@foreach.be>
2020-04-21 20:53:58 +0200

    validation address, wip :(

--- src/main/webapp/app/shop/order/customer-address.component.ts
+++ src/main/webapp/app/shop/order/customer-address.component.ts
@@ -1,27 +1,17 @@
-import { Component, EventEmitter, Input, OnInit, Output, forwardRef } from '@angular/core';
-import { ICustomer } from 'app/shared/model/customer.model';
-import { ControlValueAccessor, Validator, NG_VALIDATORS, NG_VALUE_ACCESSOR, FormControl } from '@angular/forms';
+import {Component, forwardRef, OnInit} from '@angular/core';
+import {ICustomer} from 'app/shared/model/customer.model';
+import {ControlValueAccessor, FormControl, NG_VALIDATORS, NG_VALUE_ACCESSOR, Validator} from '@angular/forms';
 
 declare var google: any;
 declare var $: any;
 
-@Component({
-    selector: 'jhi-customer-address',
-    templateUrl: './customer-address.component.html',
-    styleUrls: ['customer-order.css'],
-    providers: [
-        {
-            provide: NG_VALUE_ACCESSOR,
-            useExisting: forwardRef(() => CustomerAddressComponent),
-            multi: true
-        },
-        {
-            provide: NG_VALIDATORS,
-            useExisting: forwardRef(() => CustomerAddressComponent),
-            multi: true
-        }
-    ]
-})
+@Component( {
+                selector: 'jhi-customer-address', templateUrl: './customer-address.component.html', styleUrls: ['customer-order.css'], providers: [{
+        provide: NG_VALUE_ACCESSOR, useExisting: forwardRef( () => CustomerAddressComponent ), multi: true
+    }, {
+        provide: NG_VALIDATORS, useExisting: forwardRef( () => CustomerAddressComponent ), multi: true
+    }]
+            } )
 export class CustomerAddressComponent implements OnInit, ControlValueAccessor, Validator {
     textInput = '';
 
@@ -60,7 +50,7 @@ export class CustomerAddressComponent implements OnInit, ControlValueAccessor, V
     }
 
     onChange(event): void {
-        if (!this.textInput) {
+        if ( !this.textInput ) {
             // reset state
             this.output.street = undefined;
             this.output.city = undefined;
@@ -70,32 +60,48 @@ export class CustomerAddressComponent implements OnInit, ControlValueAccessor, V
             this.output.longitude = undefined;
         }
 
+        this.textInput = event.currentTarget.value;
+        // if ( !this.isValidAddress() ) {
+        // the address is invalid, try querying places API one more time with given input.
+        // if successful, the coordinates will be calculated and the address will be valid afterwards.
+        // TODO try to fetch suggestion here manually
+        // }
+        if ( this.isValidAddress() ) {
+            this.invalidAddressError = false;
+        } else {
+            this.invalidAddressError = true;
+        }
+
+        console.log( 'setting address to ' + this.textInput );
         // update the form
-        this.propagateChange(this.output);
+        this.propagateChange( this.output );
+    }
+
+    // passed or failed from the onChange method
+    public validate( c: FormControl ) {
+        return this.output && this.isValidAddress() ? null : {
+            invalidAddressError: {
+                valid: false
+            }
+        };
     }
 
     // returns null when valid else the validation object
     // in this case we're checking if the json parsing has
-    // passed or failed from the onChange method
-    public validate(c: FormControl) {
-        return this.output && this.output.latitude !== undefined && this.output.longitude !== null
-            ? null
-            : {
-                  invalidAddressError: {
-                      valid: false
-                  }
-              };
+
+    private isValidAddress() {
+        return this.output.latitude !== undefined && this.output.longitude !== undefined;
     }
 
     private updateAddress() {
         const place = this.searchBox.getPlace();
-        if (place != null) {
+        if ( place != null ) {
             // as defined in https://developers.google.com/maps/documentation/geocoding/intro#ReverseGeocoding
             this.output.street = place.address_components
-                .filter(attr => attr.types.filter(type => type === 'street_number' || type === 'route').length > 0)
-                .map(attr => attr.long_name)
+                .filter( attr => attr.types.filter( type => type === 'street_number' || type === 'route' ).length > 0 )
+                .map( attr => attr.long_name )
                 .reverse()
-                .join(' ');
+                .join( ' ' );
             this.output.city = place.address_components
                 .filter(attr => attr.types.filter(type => type === 'locality').length > 0)
                 .map(attr => attr.long_name)
@@ -114,12 +120,9 @@ export class CustomerAddressComponent implements OnInit, ControlValueAccessor, V
             }
             this.output.description = place.formatted_address;
             this.textInput = place.formatted_address;
-            this.invalidAddressError = false;
-        } else {
-            this.invalidAddressError = true;
         }
 
         // update the form
-        this.propagateChange(this.output);
+        this.propagateChange( this.output );
     }
 }
